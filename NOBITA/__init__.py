b4f104221f9a3c8280be61ffebc9d2b9ce4b2bc0
abhaysinghchauhan001 <chauhanabhay206243@gmail.com>
2025-06-14 01:17:22 +0530

    Add files via upload

---
+++ NOBITA/__init__.py
@@ -0,0 +1,4 @@
+from config import *
+from pyrogram import filters, Client
+
+app = Client("NOBITANguess", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
---
+++ NOBITA/__main__.py
@@ -0,0 +1,32 @@
+# NOBITA/main.py
+import asyncio
+import logging
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+from NOBITA.modules import start, nguess, autoclear
+from NOBITA import app
+
+# Setup logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+
+# Initialize Pyrogram client
+# Register handlers from modules
+start.register(app)
+nguess.register(app)
+autoclear.register(app)
+
+# Start the bot
+if __name__ == "__main__":
+    try:
+        app.run()
+    except Exception as e:
+        LOGGER.error(f"Error starting bot: {e}")
+    print("Bot started successfully!")
